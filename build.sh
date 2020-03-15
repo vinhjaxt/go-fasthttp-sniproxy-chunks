4191cfe4f6855a7c38c11c49e93fd28ccaec40c3
Vinh Duong <vinh@localhost.localdomain>
2020-03-15 21:30:07 +0700

    update

--- build.sh
+++ build.sh
@@ -2,19 +2,19 @@
 export GOOS=linux
 export GOARCH=386
 go build
-zip go-fasthttp-sniproxy-linux-386.zip domains.txt domains-regex.txt go-fasthttp-sniproxy
+zip go-fasthttp-sniproxy-chunks-linux-386.zip domains.txt domains-regex.txt go-fasthttp-sniproxy-chunks
 
 export GOOS=linux
 export GOARCH=amd64
 go build
-zip go-fasthttp-sniproxy-linux-amd64.zip domains.txt domains-regex.txt go-fasthttp-sniproxy
+zip go-fasthttp-sniproxy-chunks-linux-amd64.zip domains.txt domains-regex.txt go-fasthttp-sniproxy-chunks
 
 export GOOS=windows
 export GOARCH=386
 go build
-zip go-fasthttp-sniproxy-win32.zip domains.txt domains-regex.txt go-fasthttp-sniproxy.exe
+zip go-fasthttp-sniproxy-chunks-win32.zip domains.txt domains-regex.txt go-fasthttp-sniproxy-chunks.exe
 
 export GOOS=windows
 export GOARCH=amd64
 go build
-zip go-fasthttp-sniproxy-win64.zip domains.txt domains-regex.txt go-fasthttp-sniproxy.exe
+zip go-fasthttp-sniproxy-chunks-win64.zip domains.txt domains-regex.txt go-fasthttp-sniproxy-chunks.exe
